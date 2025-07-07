99e1a8f1c06c313ebc9a4c7bd4d8bcfeb4f76c72
Babar Mehmood <babarzaidi1122@gmail.com>
2025-07-08 02:28:05 +0500

    commit

--- tool.py
+++ tool.py
@@ -1,13 +1,28 @@
-from agents import Agent, Runner
+from agents import Agent, Runner, function_tool
 from main import config
+import os
+from dotenv import load_dotenv
+import requests
+
+
+
+load_dotenv()
+api_key = os.getenv("WEATHER_API_KEY")
+
+@function_tool
+def get_weather(city: str) -> str:
+    response = requests.get(f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}")
+    data = response.json() 
+    return f"The current weather in {city} is {data['current']['temp_c']}°C with a temperature of {data['current']['condition']['text']}."
 
 agent = Agent(
-    name="General Agent",    
+    name="Weather Agent",    
     instructions="You are a helpful assistant. Your task is to help the user with their queries.",
+    tools=[get_weather],
 )
 
 result = Runner.run_sync(agent,
-                         'who is the founder of Pakistan?',
+                         'what is the current weather in Karachi today?',
                          run_config=config,)
 
 print(result.final_output)
